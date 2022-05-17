5a377350d6410d807b981536d32ccccd98c96163
Nico Gießel <ngiessel@netik.de>
2022-05-17 14:18:52 +0200

    Update sorters

---
+++ CollectionSortingBenchmarks/Sorters/IntArray/IntArrayMergeSorter.cs
@@ -0,0 +1,62 @@
+﻿using CollectionSortingBenchmarks.Interfaces;
+
+namespace CollectionSortingBenchmarks.Sorters.IntArray
+{
+    public class IntArrayMergeSorter : IIntArraySorter
+    {
+        public void Sort(ref int[] value)
+        {
+            value = MergeSort(value, 0, value.Length - 1);
+        }
+        private int[] MergeSort(int[] input, int left, int right)
+        {
+            if (left == right)
+            {
+                int[] temp = new int[] { input[left] };
+                return temp;
+            }
+
+            int middle = left + (right - left) / 2;
+            var leftArray = MergeSort(input, left, middle);
+            var rightArray = MergeSort(input, middle + 1, right);
+            var merge = Merge(leftArray, rightArray);
+            return merge;
+        }
+
+        private int[] Merge(int[] leftArray, int[] rightArray)
+        {
+            int leftLength = leftArray.Length;
+            int rightLength = rightArray.Length;
+
+            int[] target = new int[leftLength + rightLength];
+            int targetPos = 0;
+            int leftPos = 0;
+            int rightPos = 0;
+
+            while (leftPos < leftLength && rightPos < rightLength)
+            {
+                if (leftArray[leftPos] != null && rightArray[rightPos] != null && leftArray[leftPos].CompareTo(rightArray[rightPos]) <= 0)
+                {
+                    target[targetPos++] = leftArray[leftPos];
+                    leftPos++;
+                }
+                else
+                {
+                    target[targetPos++] = rightArray[rightPos];
+                    rightPos++;
+                }
+            }
+
+            while (leftPos < leftLength)
+            {
+                target[targetPos++] = leftArray[leftPos++];
+            }
+            while (rightPos < rightLength)
+            {
+                target[targetPos++] = rightArray[rightPos++];
+            }
+            return target;
+        }
+        
+    }
+}
---
+++ CollectionSortingBenchmarks/Sorters/IntList/IntListQuickSorter.cs
@@ -0,0 +1,56 @@
+﻿using CollectionSortingBenchmarks.Interfaces;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionSortingBenchmarks.Sorters.IntList
+{
+    public class IntListQuickSorter : IIntListSorter
+    {
+        public void Sort(ref List<int> collection)
+        {
+            Quicksort(collection, 0, collection.Count - 1);
+        }
+        private void Quicksort(List<int> array, int left, int right)
+        {
+            int i = left, j = right;
+            int pivot = array[(left + right) / 2];
+
+            while (i <= j)
+            {
+                while (array[i].CompareTo(pivot) < 0)
+                {
+                    i++;
+                }
+
+                while (array[j].CompareTo(pivot) > 0)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (left < j)
+            {
+                Quicksort(array, left, j);
+            }
+
+            if (i < right)
+            {
+                Quicksort(array, i, right);
+            }
+        }
+    }
+}
